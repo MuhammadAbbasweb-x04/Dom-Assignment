e4334f561c6631804814889ba43972bccbf292f5
MuhammadAbbas-web04 <mabbasmshahi2003@gmail.com>
2025-08-05 14:04:59 +0500

    added

---
+++ app.js
@@ -0,0 +1,30 @@
+// Q1
+// Answer
+// let p = document.querySelector("#para");
+// p.innerHTML = "Welcome!"
+
+
+// Q2
+// Answer
+// let input = document.querySelector("#nameinput");
+// let head = document.querySelector("#head");
+
+// function user(){
+// head.innerHTML = input.value;
+// }
+
+
+// Q3
+// Answer
+// let inputcontainer = document.querySelector("#container");
+
+// function sec(){
+// inputcontainer.style.backgroundColor = "lightblue";
+
+// }
+
+// Q4
+// Answer
+// let maintitle = document.querySelector("mainTitle")
+// maintitle.innerHTML = "mainTitle";
+// maintitle.style.color = "red";
